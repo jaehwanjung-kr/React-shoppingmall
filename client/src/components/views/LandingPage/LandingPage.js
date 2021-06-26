bd55151cb4287329903886e0ede58cd39a69bfb9
jaehwanjung <jhnara1995@gmail.com>
2021-06-26 16:41:18 +0900

    add LoadMore Button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,16 +6,47 @@ import ImageSilder from "../../utils/ImageSilder";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState();
 
   useEffect(() => {
-    axios.post("/api/product/products").then((response) => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body) => {
+    axios.post("/api/product/products", body).then((response) => {
       if (response.data.success) {
-        setProducts(response.data.productInfo);
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
       } else {
         alert(" 상품들을 가져오는데 실패했습니다.");
       }
     });
-  }, []);
+  };
+
+  const loadMoreHandler = () => {
+    let skip = Skip + Limit;
+
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
+
+    getProducts(body);
+
+    setSkip(skip);
+  };
 
   const renderCards = Products.map((product, index) => {
     return (
@@ -31,7 +62,7 @@ function LandingPage() {
     <div style={{ width: "75%", margin: "3rem auto" }}>
       <div style={{ textAlign: "center" }}>
         <h2>
-          Let's Travel Anywhere <Icon type="rocket" />{" "}
+          Let's Travel Anywhere <Icon type="rocket" />
         </h2>
       </div>
 
@@ -43,9 +74,13 @@ function LandingPage() {
 
       <Row gutter={[16, 16]}>{renderCards}</Row>
 
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button>더보기</button>
-      </div>
+      <br />
+
+      {PostSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={loadMoreHandler}>더보기</button>
+        </div>
+      )}
     </div>
   );
 }
