b8d87840a4b9100f8d0f73daeefceef1259f15dc
jaehwanjung <jhnara1995@gmail.com>
2021-06-30 05:27:34 +0900

    add CartPage and 'UserCardBlock' section

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,6 +18,8 @@ router.get("/auth", auth, (req, res) => {
     lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+    cart: req.user.cart,
+    history: req.user.history,
   });
 });
 
@@ -72,52 +74,51 @@ router.get("/logout", auth, (req, res) => {
 router.post("/addToCart", auth, (req, res) => {
   // 먼저 User Collection에 해당 유저의 정보를 가져오기
 
-  User.findOne({ _id: req.user._id }),
-    (err, userInfo) => {
-      // 가져온 정보에서 cart에 넣으려하는 상품이 이미 들어있는지 확인
+  User.findOne({ _id: req.user._id }, (err, userInfo) => {
+    // 가져온 정보에서 cart에 넣으려하는 상품이 이미 들어있는지 확인
 
-      let duplicate = false;
+    let duplicate = false;
 
-      userInfo.cart.forEach((item) => {
-        if (item.id === req.body.productId) {
-          duplicate = ture;
-        }
-      });
-
-      // 상품이 이미 있을때
-
-      if (duplicate) {
-        User.findOneAndUpdate(
-          { id: req.user._id, "cart.id": req.body.productId },
-          { $inc: { "cart.$.quantity": 1 } },
-          { new: true },
-          (err, userInfo) => {
-            if (err) return res.status(200).json({ success: false, err });
-            res.status(400).send(userInfo.cart);
-          }
-        );
+    userInfo.cart.forEach((item) => {
+      if (item.id === req.body.productId) {
+        duplicate = true;
       }
-      // 상품을 새로 담을때
-      else {
-        User.findOneAndUpdate(
-          { id: req.user._id },
-          {
-            $push: {
-              cart: {
-                id: req.body.productId,
-                quantity: 1,
-                date: Date.now(),
-              },
+    });
+
+    // 상품이 이미 있을때
+
+    if (duplicate) {
+      User.findOneAndUpdate(
+        { _id: req.user._id, "cart.id": req.body.productId },
+        { $inc: { "cart.$.quantity": 1 } },
+        { new: true },
+        (err, userInfo) => {
+          if (err) return res.status(400).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    }
+    // 상품을 새로 담을때
+    else {
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: {
+            cart: {
+              id: req.body.productId,
+              quantity: 1,
+              date: Date.now(),
             },
           },
-          { new: true },
-          (err, userInfo) => {
-            if (err) return status(400).json({ success: false, err });
-            res.status(200).send(userInfo.cart);
-          }
-        );
-      }
-    };
+        },
+        { new: true },
+        (err, userInfo) => {
+          if (err) return status(400).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    }
+  });
 });
 
 module.exports = router;
