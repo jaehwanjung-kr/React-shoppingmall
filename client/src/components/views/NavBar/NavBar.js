d2740b50c0ff660df8297bf0cebed8f6895f055e
jaehwanjung <jhnara1995@gmail.com>
2021-07-04 23:48:03 +0900

    docs: Add Logo

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
-import LeftMenu from './Sections/LeftMenu';
-import RightMenu from './Sections/RightMenu';
-import { Drawer, Button, Icon } from 'antd';
-import './Sections/Navbar.css';
-
+import React, { useState } from "react";
+import LeftMenu from "./Sections/LeftMenu";
+import RightMenu from "./Sections/RightMenu";
+import { Drawer, Button, Icon } from "antd";
+import "./Sections/Navbar.css";
+const Logo = require("./Sections/Logo/png-transparent-travel-agent-logo-travel-text-poster-sky-thumbnail.png");
 function NavBar() {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
   const showDrawer = () => {
-    setVisible(true)
+    setVisible(true);
   };
 
   const onClose = () => {
-    setVisible(false)
+    setVisible(false);
   };
 
   return (
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+    <nav
+      className="menu"
+      style={{ position: "fixed", zIndex: 5, width: "100%" }}
+    >
       <div className="menu__logo">
-        <a href="/">Logo</a>
+        <a href="/">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{
+              width: "110%",
+              marginTop: "-68px",
+              marginLeft: "-30px",
+              marginBottom: "-25px",
+            }}
+          />
+        </a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
@@ -47,7 +61,7 @@ function NavBar() {
         </Drawer>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
